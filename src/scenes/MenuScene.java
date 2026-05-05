4506d0d9740b694e786f1bf2bbd0a2d5b82217f0
malte342developmentoffice-direktcoder <malte@shandy.de>
2026-05-05 10:27:00 +0200

    shopupdate

--- src/scenes/MenuScene.java
+++ src/scenes/MenuScene.java
@@ -8,11 +8,13 @@ public class MenuScene implements Scene {
     Button playButton;
     Button editButton;
     Button exitButton;
+    Button shopButton;
 
     public MenuScene() {
         playButton = new Button(Helpers.newVector2(20, 20), "Play", 32,Colors.BLACK, Colors.BLANK);
         editButton = new Button(Helpers.newVector2(20, 60), "Edit", 32,Colors.BLACK, Colors.BLANK);
         exitButton = new Button(Helpers.newVector2(20, 100), "Exit", 32,Colors.BLACK, Colors.BLANK);
+        shopButton = new Button(Helpers.newVector2(20, 140), "Shop", 32,Colors.RED, Colors.BLANK);
     }
 
     @Override
@@ -20,6 +22,7 @@ public class MenuScene implements Scene {
         sceneManager.addUiElement(playButton);
         sceneManager.addUiElement(editButton);
         sceneManager.addUiElement(exitButton);
+        sceneManager.addUiElement(shopButton);
     }
 
     @Override
@@ -34,6 +37,10 @@ public class MenuScene implements Scene {
 
         if (exitButton.isClicked()) {
             sm.quitGame();
+        }
+        
+        if (shopButton.isClicked()){
+            sm.changeScene(new ShopScene());
         }
     }
 
---
+++ src/scenes/ShopScene.java
@@ -0,0 +1,8 @@
+package src.scenes;
+
+public class ShopScene implements Scene{
+    
+    public void setup(SceneManager sceneManager){}
+    public void update(SceneManager sceneManager){}
+    public void draw(){}
+}
