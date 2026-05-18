04c627a6222c03e2afdc1fee0906da4e4e9c30c6
seppelin <seppelin@gmx.de>
2026-05-18 08:18:24 +0200

    quick fix

--- src/scenes/Shop.java
+++ src/scenes/Shop.java
@@ -1,15 +1,26 @@
 package src.scenes;
-import com.raylib.Raylib;
-import com.raylib.Colors;
 
-public class Shop implements Scene{
-    public Shop(){
-        
+import com.raylib.Colors;
+import com.raylib.Raylib;
+import src.ui.InputHandle;
+
+public class Shop implements SceneInterface {
+    public Shop() {
+
     }
-    
-    public void setup(SceneManager sceneManager) {}
-    public void update (SceneManager sm){}
-    public void draw(){
+
+    @Override
+    public void setup(SceneManager sceneManager) {
+
+    }
+
+    @Override
+    public void update(SceneManager sceneManager, InputHandle inputHandle) {
+
+    }
+
+    @Override
+    public void draw() {
         Raylib.DrawRectangle(400, 60, 10, 10, Colors.BLACK);
     }
 }
--- src/scenes/ShopScene.java
+++ src/scenes/ShopScene.java
@@ -1,53 +1,59 @@
 package src.scenes;
+
 import com.raylib.Colors;
-import com.raylib.Helpers;
-import src.ui.Button;
 import com.raylib.Raylib;
 import src.game.Camera;
 import src.game.Map;
 import src.game.Player;
+import src.math.Vector2I;
+import src.ui.Button;
+import src.ui.InputHandle;
 
 
-public class ShopScene implements Scene{
+public class ShopScene implements SceneInterface {
     Button MenuButton;
     Map Shopmap;
     Player player;
     Camera camera;
-    Raylib.Texture tex;    
-    
-    public ShopScene(){
-        MenuButton = new Button(Helpers.newVector2(20, 20), "Menu", 32,Colors.BLACK, Colors.BLANK);
-        Shopmap = new Map(20,20);
-        player = new Player(Shopmap.getWidth()/2, Shopmap.getHeight()/4);
+    Raylib.Texture tex;
+
+    public ShopScene() {
+        MenuButton = new Button(new Vector2I(20, 20), "Menu", 32, Colors.BLACK, Colors.BLANK);
+        Shopmap = new Map(20, 20);
+        player = new Player(Shopmap.getWidth() / 2, Shopmap.getHeight() / 4);
         camera = new Camera(100, 100, 3);
         tex = Raylib.LoadTexture("resources/Schanze.jpg");
-        
+
     }
-    
-    public void setup(SceneManager sceneManager){
+
+    public void setup(SceneManager sceneManager) {
         sceneManager.addUiElement(MenuButton);
     }
-    public void update(InputHandle ih, SceneManager sceneManager){
-        if (MenuButton.isClicked()){
+
+    @Override
+    public void update(SceneManager sceneManager, InputHandle inputHandle) {
+        if (MenuButton.isClicked()) {
             sceneManager.changeScene(new MenuScene());
         }
-        if(Raylib.IsKeyPressed(69)){
+        if (Raylib.IsKeyPressed(69)) {
             sceneManager.changeScene(new Shop());
         }
         //Shop Fenster öffnen
-        Shopmap.update();
-        player.update();
+        Shopmap.update(inputHandle, camera, (a, b) -> {
+        });
+        player.update(inputHandle, Shopmap);
         camera.target(player.getPosition());
         camera.handleResize();
-        camera.scrollZoom();
+        camera.scrollZoom(inputHandle);
     }
-    public void draw(){
-        
+
+    public void draw() {
+
         Raylib.BeginMode2D(camera);
         Raylib.DrawTexture(tex, 0, 0, Colors.WHITE);
         //Shopmap.draw();
         player.draw();
         Raylib.EndMode2D();
-        
+
     }
 }
